bffe09722e381e080578a22d3a6604b5a7b88aba
Ebram-Habib <ebramedward7@gmail.com>
2022-11-18 04:52:49 +0200

    Unit4

---
+++ Unit4/LIFO_Buff/LIFO_Buff.h
@@ -0,0 +1,31 @@
+/*
+ * LIFO_Buff.h
+ *
+ *  Created on: Nov 18, 2022
+ *      Author: Ebram Habib
+ */
+
+#ifndef LIFO_BUFF_H_
+#define LIFO_BUFF_H_
+
+//type definitions
+typedef struct {
+	unsigned int length;
+	unsigned int count;
+	unsigned int* base;
+	unsigned int* head;
+}LIFO_BUFF_t;
+
+typedef enum {
+	LIFO_NO_ERROR,
+	LIFO_FULL,
+	LIFO_EMPTY,
+	LIFO_NULL
+}LIFO_STATUS;
+
+//APIs
+LIFO_STATUS LIFO_Add_Item (LIFO_BUFF_t* lifo_buff, unsigned int item);
+LIFO_STATUS LIFO_Get_Item (LIFO_BUFF_t* lifo_buff, unsigned int* item);
+LIFO_STATUS LIFO_Init (LIFO_BUFF_t* lifo_buff, unsigned int* buf, unsigned int length);
+
+#endif /* LIFO_BUFF_H_ */
